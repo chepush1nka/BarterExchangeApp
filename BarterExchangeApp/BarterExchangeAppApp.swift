e5e3fabcb5101b5fd3bba2d1ec5835736e40ccf3
pvvyaltsev <pvvyaltsev@yandex-team.ru>
2024-01-31 14:28:27 +0300

    Initial Commit

---
+++ BarterExchangeApp/BarterExchangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarterExchangeAppApp.swift
+//  BarterExchangeApp
+//
+//  Created by Pavel Vyaltsev on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarterExchangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
